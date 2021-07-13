9e731dcc26bf20c3682ebbfcd530e31c86093995
Michal Drabik <micdra7@gmail.com>
2021-07-13 14:05:44 +0200

    feat: add custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './components/App/App';
+import theme from './theme';
+import '@fontsource/lato/700.css';
+import '@fontsource/questrial/400.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
