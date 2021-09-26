0b1f1b311c60ad1af99d41e5e1e71637c400b874
Michal Drabik <micdra7@gmail.com>
2021-09-26 15:53:17 +0200

    feat: prepare message box

--- src/pages/client/Chats/components/Chatroom.tsx
+++ src/pages/client/Chats/components/Chatroom.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { API } from 'services/api';
-import { Box, Grid } from '@chakra-ui/react';
+import { Box, Grid, Textarea, IconButton, Icon } from '@chakra-ui/react';
 import { Loader } from 'components';
+import { FaPaperPlane } from 'react-icons/fa';
 
 const Chatroom = (): JSX.Element => {
+  const [limit, setLimit] = useState(10);
+  const [message, setMessage] = useState('');
+
   const { chatroomId }: { chatroomId: string } = useParams();
   const { isLoading: chatroomLoading, data: chatroom } = useQuery(
     `/chatrooms/${chatroomId}`,
     () => API.get(`/chatrooms/${chatroomId}`),
   );
   const { isLoading: messagesLoading, data: messages } = useQuery(
-    `/chatrooms/messages/${chatroomId}`,
-    () => API.get(`/chatrooms/messages/${chatroomId}`),
+    `/chatrooms/messages/${chatroomId}?limit=${limit}`,
+    () => API.get(`/chatrooms/messages/${chatroomId}?limit=${limit}`),
   );
 
   return (
     <Box>
-      {chatroomLoading && messagesLoading ? <Loader /> : <Grid>xd</Grid>}
+      {chatroomLoading && messagesLoading ? (
+        <Loader />
+      ) : (
+        <Grid templateColumns={['1fr']}>
+          <Grid
+            p={4}
+            w="100%"
+            minH="120px"
+            templateColumns="1fr 0.25fr"
+            gap="0.5rem">
+            <Textarea
+              value={message}
+              onChange={event => setMessage(event.target.value)}
+              placeholder="Type your message and press ENTER to send"
+              h="100%"
+              resize="none"
+            />
+            <IconButton
+              colorScheme="cyan"
+              aria-label="Send"
+              rounded="md"
+              h="100%"
+              icon={<Icon as={FaPaperPlane} color="white" />}
+            />
+          </Grid>
+        </Grid>
+      )}
     </Box>
   );
 };
