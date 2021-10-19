e2a9e6bd84f10b5916732c79e7f5c7714a8964e4
Michal Drabik <micdra7@gmail.com>
2021-10-19 16:52:54 +0200

    feat: rework message rendering process

---
+++ src/types/Message.ts
@@ -0,0 +1,10 @@
+export type TMessage = {
+  id: number;
+  chatroomId: number;
+  userId: number;
+  chatroomName: string;
+  content: string;
+  sendTime: string;
+  userEmail: string;
+  userFullName: string;
+};
