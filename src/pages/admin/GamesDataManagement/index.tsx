8c112331a0f24544b1ef73635a307e7560ea2146
Michal Drabik <micdra7@gmail.com>
2021-10-26 21:23:06 +0200

    feat: WIP game data list

--- src/pages/admin/GamesDataManagement/index.tsx
+++ src/pages/admin/GamesDataManagement/index.tsx
@@ -1,10 +1,12 @@
 import { WideContentPage } from 'components';
 import React from 'react';
 import { Text } from '@chakra-ui/react';
+import GameDataList from './components/GameDataList';
 
 const GamesDataManagement = (): JSX.Element => (
   <WideContentPage title="Game Data Management">
     <Text mb={6}>Edit existing or add new data to your games</Text>
+    <GameDataList />
   </WideContentPage>
 );
 
