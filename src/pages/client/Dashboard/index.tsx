3c22a7770f808bff04d3474bcb3737969ebf4ee8
Michal Drabik <micdra7@gmail.com>
2021-10-28 21:48:58 +0200

    chore: remove remote component from dashboard

--- src/pages/client/Dashboard/index.tsx
+++ src/pages/client/Dashboard/index.tsx
@@ -1,15 +1,9 @@
 import { WideContentPage } from 'components';
 import React from 'react';
-import { RemoteComponent } from '@paciolan/remote-component';
-
-const url = 'http://localhost:5000/test.js';
-
-const MyComponent = () => <RemoteComponent url={url} />;
 
 const Dashboard = (): JSX.Element => (
   <WideContentPage title="Dashboard">
     <span>Dashboard</span>
-    <MyComponent />
   </WideContentPage>
 );
 
