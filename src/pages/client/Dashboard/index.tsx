e122ea4b07103b61f9bba18a92a40719961477c6
Michal Drabik <micdra7@gmail.com>
2021-10-30 20:47:30 +0200

    feat: add tasks list

--- src/pages/client/Dashboard/index.tsx
+++ src/pages/client/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { WideContentPage } from 'components';
 import React from 'react';
+import FinishingTasks from './components/FinishingTasks';
 import UpcomingCallsList from './components/UpcomingCallsList';
 
 const Dashboard = (): JSX.Element => (
   <WideContentPage title="Dashboard">
     <span>View your upcoming calls and tasks with upcoming finish date</span>
     <UpcomingCallsList />
+    <FinishingTasks />
   </WideContentPage>
 );
 
